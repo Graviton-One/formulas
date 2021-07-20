1ac8ab03c4383da8150bfd758a3c843ff5399473
Hilly Williams <shamfama@gmail.com>
2021-07-20 11:58:18 +0000

    Rework output.sh

--- output.sh
+++ output.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 
-protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=./formulas ./formulas/formulas.proto
+namespaces=(constants formulas)
+
+
+for ns in ${namespaces[@]}
+do
+  # protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=. ./formulas/formulas.proto
+  protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=. "./$ns/$ns.proto"
+done
+
 # protoc --proto_path=./formulas --go_out=./formulas ./formulas/formulas.proto
 
